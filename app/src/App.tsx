df0d4b7a07f19b6e787285d1549b12fa6b376cdb
André Ferreira <1230168@isep.ipp.pt>
2025-03-26 07:24:51 +0000

    Add layout components and theme customizations for EnergyOne application

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,35 +2,69 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from "@mui/material";
-import Header from './components/Header';
+import { Button, Stack } from "@mui/material";
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import SideMenu from './components/SideMenu';
+import AppTheme from './components/AppTheme';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header></Header>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)} variant="contained" color="secondary">
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppTheme>
+        <Box sx={{ flexDirection: 'column', display: 'flex', height: '100dvh' }}>
+          <Box sx={{ flex: '1 1', overflow: 'auto' }}>
+            <Box sx={{ display: 'flex' }}>
+              <SideMenu />
+              <Box
+                component="main"
+                sx={(theme) => ({
+                  flexGrow: 1,
+                  backgroundColor: alpha(theme.palette.background.default, 1),
+                  overflow: 'auto',
+                })}
+              >
+                <Stack
+                  spacing={2}
+                  sx={{
+                    alignItems: 'center',
+                    mx: 3,
+                    pb: 5,
+                    mt: { xs: 8, md: 0 },
+                  }}
+                >
+                  <div>
+                    <a href="https://vite.dev" target="_blank">
+                      <img src={viteLogo} className="logo" alt="Vite logo" />
+                    </a>
+                    <a href="https://react.dev" target="_blank">
+                      <img src={reactLogo} className="logo react" alt="React logo" />
+                    </a>
+                  </div>
+                  <h1>Vite + React</h1>
+                  <div className="card">
+                    <Button sx={{ backgroundColor: 'hsl(220, 30%, 7%)' }} onClick={() => setCount((count) => count + 1)} variant="contained" color="secondary">
+                      count is {count}
+                    </Button>
+                    <p>
+                      Edit <code>src/App.tsx</code> and save to test HMR
+                    </p>
+                  </div>
+                  <p className="read-the-docs">
+                    Click on the Vite and React logos to learn more
+                  </p>
+                  <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                  <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                  <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                  <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+                </Stack>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </AppTheme>
     </>
   )
 }
